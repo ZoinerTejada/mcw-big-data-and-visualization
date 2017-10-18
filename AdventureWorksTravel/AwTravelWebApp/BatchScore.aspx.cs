2eb979533b88db774d6722a34dfbcc9db053ec52
kylebunting <kyle.bunting@gmail.com>
2017-10-17 23:02:15 -0400

    Added AdventureWorksTravel solution files

---
+++ AdventureWorksTravel/AwTravelWebApp/BatchScore.aspx.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PBIWebApp
+{
+    public partial class BatchScore : System.Web.UI.Page
+    {
+
+        string baseUri = "https://{0}.services.azureml.net/workspaces/{1}/services/{2}/jobs";
+        PBIWebApp.Properties.Settings settings = PBIWebApp.Properties.Settings.Default;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnSubmitJob_Click(object sender, EventArgs e)
+        {
+            SubmitJob().Wait();
+        }
+
+        protected void btnStartJob_Click(object sender, EventArgs e)
+        {
+            StartJob().Wait();
+        }
+
+        protected void btnCheckJobStatus_Click(object sender, EventArgs e)
+        {
+            GetJobStatus().Wait();
+        }
+
+        private async Task SubmitJob()
+        {
+            string responseContent = string.Empty;
+            
+            string submitUri = string.Format(baseUri, settings.ML_RegionPrefix, settings.ML_WorkspaceID, settings.ML_ServiceID);
+
+            System.Net.WebClient webClient = new System.Net.WebClient();
+
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(";BlobEndpoint.*");
+            string blobConnString = regex.Replace(settings.ML_StorageAccount, "");
+
+            using (HttpClient client = new HttpClient())
+            {
+                var request = new BatchExecutionRequest()
+                {
+
+                    Input = new AzureBlobDataReference()
+                    {
+                        ConnectionString = blobConnString,
+                        RelativeLocation = txtSourceRelativeLocation.Text
+                    },
+
+                    Outputs = new Dictionary<string, AzureBlobDataReference>()
+                    {
+                        {
+                            "output1",
+                            new AzureBlobDataReference()
+                            {
+                                ConnectionString = blobConnString,
+                                RelativeLocation = txtDestRelativeLocation.Text
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.ML_APIKey);
+
+                var response = await client.PostAsJsonAsync(submitUri + "?api-version=2.0", request).ConfigureAwait(false);
+                if (!response.IsSuccessStatusCode)
+                {
+                    //await WriteFailedResponse(response);
+                    return;
+                }
+
+                string jobId = await response.Content.ReadAsAsync<string>();
+
+                txtJobID.Text = jobId;
+
+            }  
+        }
+
+        private async Task StartJob()
+        {
+            string startUri = string.Format(baseUri, settings.ML_RegionPrefix, settings.ML_WorkspaceID, settings.ML_ServiceID);
+
+            System.Net.WebClient webClient = new System.Net.WebClient();
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.ML_APIKey);
+                var response = await client.PostAsync(startUri + "/" + txtJobID.Text + "/start?api-version=2.0", null).ConfigureAwait(false);
+                if (!response.IsSuccessStatusCode)
+                {
+                    txtJobStatus.Text = "Error. " + response.ReasonPhrase;
+                    return;
+                }
+            }
+        }
+
+        private async Task GetJobStatus()
+        {
+            long TimeOutInMilliseconds = 10000;
+
+            string jobStatusUri = string.Format(baseUri, settings.ML_RegionPrefix, settings.ML_WorkspaceID, settings.ML_ServiceID) + "/" + txtJobID.Text + "?api-version=2.0";
+            string jobId = txtJobID.Text;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings.ML_APIKey);
+
+                //Stopwatch watch = Stopwatch.StartNew();
+                bool done = false;
+                //while (!done)
+                //{
+                    var response = await client.GetAsync(jobStatusUri).ConfigureAwait(false);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+
+                        txtJobStatus.Text = "Error. " + response.ReasonPhrase;
+                        return;
+                    }
+
+                    BatchScoreStatus status = await response.Content.ReadAsAsync<BatchScoreStatus>();
+
+                    //if (watch.ElapsedMilliseconds > TimeOutInMilliseconds)
+                    //{
+                    //    done = true;
+                    //    Console.WriteLine(string.Format("Timed out. Deleting job {0} ...", jobId));
+                    //    await client.DeleteAsync(jobStatusUri);
+                    //}
+
+                    switch (status.StatusCode)
+                    {
+                        case BatchScoreStatusCode.NotStarted:
+                            //Console.WriteLine(string.Format("Job {0} not yet started...", jobId));
+                            txtJobStatus.Text = "Job not yet started.";
+                            break;
+                        case BatchScoreStatusCode.Running:
+                            //Console.WriteLine(string.Format("Job {0} running...", jobId));
+                            txtJobStatus.Text = "Job is running.";
+                            break;
+                        case BatchScoreStatusCode.Failed:
+                            //Console.WriteLine(string.Format("Job {0} failed!", jobId));
+                            //Console.WriteLine(string.Format("Error details: {0}", status.Details));
+                            txtJobStatus.Text = "Job failed. Error details" + status.Details;
+                            done = true;
+                            break;
+                        case BatchScoreStatusCode.Cancelled:
+                            //Console.WriteLine(string.Format("Job {0} cancelled!", jobId));
+                            txtJobStatus.Text = "Job was cancelled";
+                            done = true;
+                            break;
+                        case BatchScoreStatusCode.Finished:
+                            done = true;
+                            //Console.WriteLine(string.Format("Job {0} finished!", jobId));
+
+                            txtJobStatus.Text = "Job is finished.";
+                            
+                            break;
+                    }
+
+                    //if (!done)
+                    //{
+                    //    Thread.Sleep(1000); // Wait one second
+                    //}
+                //}
+            }
+        }
+
+        public class AzureBlobDataReference
+        {
+            // Storage connection string used for regular blobs. It has the following format:
+            // DefaultEndpointsProtocol=https;AccountName=ACCOUNT_NAME;AccountKey=ACCOUNT_KEY
+            // It's not used for shared access signature blobs.
+            public string ConnectionString { get; set; }
+
+            // Relative uri for the blob, used for regular blobs as well as shared access 
+            // signature blobs.
+            public string RelativeLocation { get; set; }
+
+            // Base url, only used for shared access signature blobs.
+            public string BaseLocation { get; set; }
+
+            // Shared access signature, only used for shared access signature blobs.
+            public string SasBlobToken { get; set; }
+        }
+
+        public enum BatchScoreStatusCode
+        {
+            NotStarted,
+            Running,
+            Failed,
+            Cancelled,
+            Finished
+        }
+
+        public class BatchScoreStatus
+        {
+            // Status code for the batch scoring job
+            public BatchScoreStatusCode StatusCode { get; set; }
+
+
+            // Locations for the potential multiple batch scoring outputs
+            public IDictionary<string, AzureBlobDataReference> Results { get; set; }
+
+            // Error details, if any
+            public string Details { get; set; }
+        }
+
+        public class BatchExecutionRequest
+        {
+            public AzureBlobDataReference Input { get; set; }
+            public IDictionary<string, string> GlobalParameters { get; set; }
+
+            // Locations for the potential multiple batch scoring outputs
+            public IDictionary<string, AzureBlobDataReference> Outputs { get; set; }
+        }
+
+
+    }
+}
---
+++ AdventureWorksTravel/AwTravelWebApp/Main.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="PBIWebApp.Main" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+
+        <!-- Embed tiles / map -->
+        <div class="col-md-6">
+            <div class="panel panel-default shadow-z-2">
+                <div class="panel-body" style="height:450px;">
+                    
+                    <asp:Button ID="signInButton" class="btn btn-primary" runat="server" OnClick="signInButton_Click" Text="Sign in to Power BI" />
+                    
+                    <asp:Panel ID="mapPanel" runat="server" Visible="false">
+                        <iframe id="iFrameEmbedTile" src="" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+                    </asp:Panel>
+
+                    <asp:Panel ID="signInStatus" runat="server" Visible="false">
+                        <div class="form-group m-b-30">
+                            <p>Signed in as</p>
+                            <asp:Label ID="userLabel" runat="server"></asp:Label>
+                        </div>
+                        <div class="form-group m-b-30" style="visibility:hidden">
+                            <p>Access Token</p>
+                            <asp:TextBox ID="accessTokenTextbox" class="form-control" runat="server" Width="586px" ></asp:TextBox>
+                        </div>
+                        <div class="form-group m-b-30" style="visibility:hidden">
+                            <p>iFrame URL</p>
+                            <asp:TextBox ID="txtIFrameURL" class="form-control" runat="server" Width="586px" ClientIDMode="Static"></asp:TextBox>
+                        </div>
+
+                    </asp:Panel>
+
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3">
+            <%--<div class="panel panel-default no-bd">--%>
+            <div class="panel panel-transparent no-bd">
+
+                <div class="panel-body bg-white">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <asp:Panel ID="PanelPredictInputs" runat="server" Visible="true">
+                                <div class="form-group m-b-30">
+                                    <p>From</p>
+                                    <asp:DropDownList ID="ddlOriginAirportCode" class="form-control" runat="server"></asp:DropDownList>
+                                </div>
+                                <div class="form-group m-b-30">
+                                    <p>To</p>
+                                    <asp:DropDownList ID="ddlDestAirportCode" class="form-control" runat="server"></asp:DropDownList>
+                                </div>
+                                <div class="form-group m-b-30">
+                                    <p>Date</p>
+                                    <asp:TextBox runat="server" class="form-control" type="text" id="txtDepartureDate" value="11/10/2015" />
+                                </div>
+                                <div class="form-group m-b-30">
+                                    <p>Time</p>
+                                    <asp:TextBox runat="server" class="form-control" type="text" id="txtDepartureHour" value="19" />
+                                </div>
+                                <div class="form-group m-b-30">
+                                    <p>Carrier</p>
+                                    <asp:TextBox runat="server" class="form-control" type="text" id="txtCarrier" value="DL" />
+                                </div>
+                                <asp:Button ID="btnPredictDelays" class="btn btn-primary" runat="server" Text="Predict Delay" OnClick="btnPredictDelays_Click" />
+                            </asp:Panel>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="panel panel-transparent no-bd">
+                <div class="panel-body bg-white">
+                    <asp:Panel ID="PanelPredictResults" runat="server" Visible="true">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Weather Forecast</h3>
+                                <asp:Image ID="weatherForecast" runat="server" />
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3>Delay Prediction</h3>
+                                <div class="prediction prediction-mid shadow-z-1">
+                                    <div id="predictionDelay">
+                                        <p>
+                                            <asp:Label ID="lblPrediction" runat="server" Text=""></asp:Label>
+                                        </p>
+                                        (<asp:Label ID="lblConfidence" runat="server" Text=""></asp:Label>% confidence)
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </asp:Panel>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</asp:Content>
